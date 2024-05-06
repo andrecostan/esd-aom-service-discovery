9ecd16f0bd0c2c6ad82940749e35d631f3e6ddcc
André Costa <andrecostan@gmail.com>
2024-05-06 16:43:09 -0300

    First commit

---
+++ src/test/java/uece/esd/aom/esdaomservicediscovery/EsdAomServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package uece.esd.aom.esdaomservicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsdAomServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
